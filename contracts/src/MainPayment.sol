a21622cc1aea471a2af2ec41ebff616b01dfb7e6
megabyte0x <yashygkf@gmail.com>
2024-01-17 06:28:06 +0530

    feat: init Partner Payment

--- contracts/src/MainPayment.sol
+++ contracts/src/MainPayment.sol
@@ -6,12 +6,15 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {FixedPointMathLib} from "@solmate/contracts/utils/FixedPointMathLib.sol";
 
+import {Utils} from "./Utils.sol";
+
 contract MainPayment is Ownable {
     error MainPayment__ZeroAmount();
-    error MainPayment__NotEnoughGP(uint256 _gpAmountLeft, uint256 _gpRewardAmount);
     error MainPayment__ZeroAddress();
     error MainPayment__NotEnoughGHO();
     error MainPayment__OnlyGHOPassportHolders();
+    error MainPayment__OnlyPartnerBookingContract();
+    error MainPayment__NotEnoughGP(uint256 _gpAmountLeft, uint256 _gpRewardAmount);
 
     event MainPayment__PaidWithGHO(address indexed _recipient, uint256 _amount);
     event MainPayment__GPRewarded(address indexed _user, uint256 indexed _amountPaid, uint256 indexed _gpAmountLeft);
@@ -25,11 +28,21 @@ contract MainPayment is Ownable {
       \__ \ || (_| | ||  __/  \ V / (_| | |  | | (_| | |_) |  __/\__ \
       |___/\__\__,_|\__\___|   \_/ \__,_|_|  |_|\__,_|_.__/ \___||___/
     */
-    IERC721 public s_ghoPassport;
-    ERC20 public s_ghoToken;
+
+    Utils public s_utils;
     ERC20 public s_gpToken;
+    ERC20 public s_ghoToken;
+    IERC721 public s_ghoPassport;
 
     uint256 public s_minimumAmt;
+
+    constructor(address _ghoToken, address _gpToken, address _ghoPassport, address _utils, uint256 _minimumAmt) {
+        s_ghoToken = ERC20(_ghoToken);
+        s_gpToken = ERC20(_gpToken);
+        s_ghoPassport = IERC721(_ghoPassport);
+        s_utils = Utils(_utils);
+        s_minimumAmt = _minimumAmt;
+    }
 
     /*
                            _ _  __ _
@@ -51,6 +64,11 @@ contract MainPayment is Ownable {
 
     modifier isGHOPassportHolder() {
         if (!(s_ghoPassport.balanceOf(msg.sender) > 0)) revert MainPayment__OnlyGHOPassportHolders();
+        _;
+    }
+
+    modifier isPartnerBookingContract() {
+        if (!(s_utils.isPartnerBookingContractVaild(msg.sender))) revert MainPayment__OnlyPartnerBookingContract();
         _;
     }
 
@@ -79,6 +97,37 @@ contract MainPayment is Ownable {
         isZeroAmount(_amount)
     {
         address _user = msg.sender;
+        uint256 gpAmount = s_gpToken.balanceOf(address(this));
+
+        if (s_ghoToken.balanceOf(_user) < _amount) revert MainPayment__NotEnoughGHO();
+
+        uint256 gpRewardAmount = _calculateGPReward(_amount);
+        if (gpAmount < gpRewardAmount) revert MainPayment__NotEnoughGP(gpAmount, gpRewardAmount);
+
+        // transfer funds to the recipient
+        s_ghoToken.transferFrom(_user, _recipient, _amount);
+        emit MainPayment__PaidWithGHO(_recipient, _amount);
+
+        // transfer GP to _user
+        s_gpToken.transfer(_user, gpRewardAmount);
+        emit MainPayment__GPRewarded(_user, gpRewardAmount, (gpAmount - gpRewardAmount));
+    }
+
+    /**
+     * Function to pay with GHO and earn GP.
+     * @param _recipient The address to receive the GHO tokens
+     * @param _amount The amount of GHO tokens to pay
+     * @param _sender The address of the sender
+     * @notice This function is for partner booking contracts only.
+     */
+
+    function payWithGHO(address _sender, address _recipient, uint256 _amount)
+        public
+        isPartnerBookingContract
+        isZeroAdrress(_recipient)
+        isZeroAmount(_amount)
+    {
+        address _user = _sender;
         uint256 gpAmount = s_gpToken.balanceOf(address(this));
 
         if (s_ghoToken.balanceOf(_user) < _amount) revert MainPayment__NotEnoughGHO();
