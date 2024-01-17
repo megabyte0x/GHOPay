7126bde9a8a19d39fad3c894f81a5521e980c523
megabyte0x <yashygkf@gmail.com>
2024-01-18 04:03:14 +0530

    feat: Deploy Test GHO

---
+++ contracts/script/DeployTestGHO.s.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
+import {TestGHO} from "../src/TestGHO.sol";
+
+contract DeployTestGHO is Script {
+    function run() public returns (address ghoToken) {
+        vm.startBroadcast();
+        TestGHO _ghoToken = new TestGHO();
+        ghoToken = address(_ghoToken);
+        console2.log("TestGHO deployed at: %s", ghoToken);
+        vm.stopBroadcast();
+    }
+}
