ddbd11a01f681aa5803feedac8e3023d7450d59c
megabyte0x <yashygkf@gmail.com>
2024-01-18 04:03:01 +0530

    edit: return value added

--- contracts/script/DeployMainVault.s.sol
+++ contracts/script/DeployMainVault.s.sol
@@ -9,22 +9,23 @@ import {MainVault} from "../src/MainVault.sol";
 import {HelperConfig} from "../script/HelperConfig.s.sol";
 
 contract DeployMainVault is Script {
-    function deployMainVault(address _ghoToken, uint256 _mainDeployerKey) public {
+    function deployMainVault(address _ghoToken, uint256 _mainDeployerKey) public returns (address mainVault) {
         vm.startBroadcast(_mainDeployerKey);
         MainVault _mainVault = new MainVault(ERC20(_ghoToken));
         vm.stopBroadcast();
 
+        mainVault = address(_mainVault);
         console2.log("MainVault deployed at: %s", address(_mainVault));
     }
 
-    function deployMainVaultUsingConfigs() public {
+    function deployMainVaultUsingConfigs() public returns (address mainVault) {
         HelperConfig _helperConfigs = new HelperConfig();
         address _ghoToken = _helperConfigs.s_ghoToken();
         uint256 _mainDeployerKey = _helperConfigs.s_mainDeployerKey();
-        deployMainVault(_ghoToken, _mainDeployerKey);
+        mainVault = deployMainVault(_ghoToken, _mainDeployerKey);
     }
 
-    function run() public {
-        deployMainVaultUsingConfigs();
+    function run() public returns (address mainVault) {
+        mainVault = deployMainVaultUsingConfigs();
     }
 }
