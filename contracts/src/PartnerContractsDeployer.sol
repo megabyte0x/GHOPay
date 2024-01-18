46c765bdd3d606cd29e43cf39920717431d9e02c
lucifer0x17 <ayush.aa347@gmail.com>
2024-01-19 03:31:28 +0530

    modules

---
+++ contracts/src/PartnerContractsDeployer.sol
@@ -0,0 +1,57 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20} from "./ERC4626Flatten.sol";
+
+import {PartnerVault} from "./PartnerVault.sol";
+import {PartnerPayment} from "./PartnerPayment.sol";
+
+contract PartnerContractsDeployer is Ownable {
+    address public s_utilsContract;
+    address public i_owner;
+    address public s_mainPayment;
+
+    mapping(address => PartnerDetails) public s_addressToPartnerDetails;
+    address[] public s_partneres;
+
+    struct PartnerDetails {
+        address s_vault;
+        address s_partnerPayment;
+    }
+
+    constructor(address _owner, address _mainPayment) Ownable(_owner) {
+        i_owner = _owner;
+        s_mainPayment = _mainPayment;
+    }
+
+    function createVault(
+        ERC20 _ghoToken,
+        string memory _name,
+        string memory _symbol,
+        uint8 _ratio,
+        uint8 _maxAmtPercentInRp
+    ) public {
+        PartnerVault partnerVault = new PartnerVault(
+            _ghoToken,
+            _name,
+            _symbol,
+            msg.sender,
+            _ratio
+        );
+        PartnerPayment partnerPayment = new PartnerPayment(
+            address(partnerVault),
+            s_mainPayment,
+            msg.sender,
+            _ratio,
+            _maxAmtPercentInRp
+        );
+
+        s_addressToPartnerDetails[msg.sender] = PartnerDetails({
+            s_vault: address(partnerVault),
+            s_partnerPayment: address(partnerPayment)
+        });
+
+        s_partneres.push(msg.sender);
+    }
+}
